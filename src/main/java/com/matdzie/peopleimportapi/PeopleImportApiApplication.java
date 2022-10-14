ac44f9552d503a146c2cd236afe7dab6451bf48d
Mateusz Dziedzic <mateuszdziedzic90@gmail.com>
2022-10-14 18:01:58 +0200

    Initial commit - generated with https://start.spring.io/

---
+++ src/main/java/com/matdzie/peopleimportapi/PeopleImportApiApplication.java
@@ -0,0 +1,13 @@
+package com.matdzie.peopleimportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleImportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleImportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matdzie/peopleimportapi/PeopleImportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matdzie.peopleimportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleImportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
